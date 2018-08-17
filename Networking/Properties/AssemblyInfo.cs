f57ce0cafcef5d0b514541642ad3aed32cd666d3
Sean Newman <seangnewman@msn.com>
2018-08-16 22:36:44 -0700

    Completed Networking examples

---
+++ Networking/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Networking")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Networking")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd627379-9b02-4374-a36e-0a99ae11da1f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Networking/Test_Download.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Networking
+{
+    [TestClass]
+    public class Test_Download
+    {
+        string url = "http://www.deelay.me/10000/http://www.delsink.com";
+
+        [TestMethod]
+        public void Test_Download_DelsinkCOM()
+        {
+            var httpRequestInfo = HttpWebRequest.CreateHttp(url);
+            var callback = new AsyncCallback(HttpResponseAvailable);
+
+            var ar = httpRequestInfo.BeginGetResponse(callback, httpRequestInfo) ;   //initial round trip to server
+
+            ar.AsyncWaitHandle.WaitOne();
+        }
+
+        private static void HttpResponseAvailable(IAsyncResult ar)
+        {
+            // Running asyncronously on background thread
+            var httpRequestInfo = ar.AsyncState as HttpWebRequest;
+            var httpResponseInfo = httpRequestInfo.EndGetResponse(ar) as HttpWebResponse;
+
+            var responseStream = httpResponseInfo.GetResponseStream();  // downloading page content
+
+            using (var sr = new StreamReader(responseStream))
+            {
+                var webPage = sr.ReadToEnd();
+            }
+
+
+        }
+    }
+}
