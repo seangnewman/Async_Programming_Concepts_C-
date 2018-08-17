c0cd6fc7ac9d210de0680fe4a9fff2992977d854
Sean Newman <seangnewman@msn.com>
2018-08-17 06:30:24 -0700

    Database simulaton

---
+++ Databases/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Databases")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Databases")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25d17de4-0d7f-4b63-a27b-219f82a3904a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Databases/Test_Databases.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.SqlClient;
+
+namespace Databases
+{
+    [TestClass]
+    public class Test_Databases
+    {
+        [TestMethod]
+        public void Test_DB_Sync()
+        {
+            string connectionString;
+            connectionString = "Data Source=ALTAIR\\ALTAIR_2014;Initial Catalog=master;Integrated Security=True";
+
+            string sqlSelect = "SELECT @@VERSION";
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                using (var sqlCommand = new SqlCommand(sqlSelect, sqlConnection))
+                {
+                    using (var reader = sqlCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var data = reader[0].ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Test_DB_ASync()
+        {
+            string connectionString;
+            connectionString = "Data Source=ALTAIR\\ALTAIR_2014;Initial Catalog=master;Integrated Security=True";
+
+            string sqlSelect = "SELECT @@VERSION";
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+
+                var sqlCommand = new SqlCommand(sqlSelect, sqlConnection);
+
+                var callback = new AsyncCallback(DataAvailable);
+                var ar = sqlCommand.BeginExecuteReader(callback, sqlCommand);
+               
+
+                ar.AsyncWaitHandle.WaitOne();
+            }
+
+           
+        }
+        private static void DataAvailable(IAsyncResult ar)
+        {
+            var sqlCommand = ar.AsyncState as SqlCommand;
+
+            using (var reader = sqlCommand.EndExecuteReader(ar))
+            {
+                while (reader.Read())
+                {
+                    var data = reader[0].ToString();
+                }
+            }
+         }
+
+
+    }
+}
