d743c55d9846db21279cb568c5e9ac50561b92e0
Sean Newman <seangnewman@msn.com>
2018-08-18 02:18:06 -0700

    Overview of Threads

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -12,16 +12,47 @@ namespace Threads
         static void Main(string[] args)
         {
             Console.WriteLine("Starting in main method");
-            var t = new Thread(new ThreadStart(DoWork));
-            t.Start();
-            Thread.Sleep(1000);
+
+            var t1 = new Thread(new ThreadStart(AddToList));
+             t1.Start();
+
+            var t2 = new Thread(new ThreadStart(AddToList));
+            t2.Start();
+
+            var t3 = new Thread(new ThreadStart(AddToList));
+            t3.Start();
+
             Console.WriteLine("Ending in main method");
         }
-
-        private static void DoWork()
+        private static object myLock = new object();
+        private static List<int> numbers = new List<int>();
+        private static void AddToList()
         {
-            Console.WriteLine("Work to Do!");
-            Thread.Sleep(1000);
+            
+                for (int ix = 0; ix < 100; ix++)
+                {
+                Thread.Sleep(rnd.Next(1, 500));
+                  lock (myLock)
+                  {
+                      numbers.Add(ix);
+                  }
+                   
+                }
+             
+     
         }
+
+        private static Random rnd = new Random();
+        private static int total = 0;
+
+        //private static void DoWork()
+        //{
+        //    Thread.Sleep(rnd.Next(1, 1500));
+        //    int myTotal  = total;
+        //    Thread.Sleep(rnd.Next(1, 1500));
+        //    total = myTotal + 1;
+        //    Console.WriteLine("Managed Thread Id : {0} says {1}", Thread.CurrentThread.ManagedThreadId,  total);
+            
+        //}
     }
 }
