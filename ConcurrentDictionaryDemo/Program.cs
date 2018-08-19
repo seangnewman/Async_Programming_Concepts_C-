529c44673165408a99b4dd1cdb48e6bcfa4c8965
Sean Newman <seangnewman@msn.com>
2018-08-19 11:42:49 -0700

    Completed Course

---
+++ ConcurrentDictionaryDemo/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentDictionaryDemo
+{
+    class Program
+    {
+        static ConcurrentDictionary<int, string> cd = new ConcurrentDictionary<int, string>();
+        static void Main(string[] args)
+        {
+            //if (cd.TryAdd(1, "one"))
+            //{
+            //    Console.WriteLine("KVP 1 was added");
+            //}
+
+            //string val = cd.GetOrAdd(1, "_ONE_");
+            //Console.WriteLine("Existing 1: {0}", val);
+
+            //string val2 = cd.AddOrUpdate(1, "ONE", (int i, string existingValue) =>
+            //{
+            //    return existingValue.ToUpper();
+            //});
+
+            //Console.WriteLine("Existing 1: {0}", val2);
+
+            //if(cd.TryGetValue(1, out string val3))
+            //{
+            //    Console.WriteLine("Existing 1: {0}",val3);
+            //}
+
+            string filename = @"C:\Users\Sean\Source\Repos\Programming_Concepts\ConcurrentDictionaryDemo/sp_test.txt";
+            string[] lines = File.ReadAllLines(filename);
+
+            Parallel.ForEach<string>(lines, (string line) => {
+                string[] words = line.Split(' ');
+                foreach(string word in words)
+                {
+                    if(String.IsNullOrWhiteSpace(word))
+                    {
+                        continue;
+                    }
+
+                    wordCount.AddOrUpdate(word, 1, (k, currentCount) =>
+                    {
+                        return currentCount + 1;
+                    });
+                }
+            });
+
+
+        }
+       static  ConcurrentDictionary<string, uint> wordCount = new ConcurrentDictionary<string, uint>();
+    }
+}
---
+++ ProducerConsumerExample/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumerExample
+{
+    
+
+    class Program
+    {
+        static Random rnd = new Random();
+       // static ConcurrentQueue<ulong> cq = new ConcurrentQueue<ulong>();
+
+        static BlockingCollection<ulong> numbers = new BlockingCollection<ulong>(10);
+        static void Main(string[] args)
+        {
+            Thread threadFib = new Thread(new ThreadStart(GenerateFib));
+            threadFib.IsBackground = false;
+            threadFib.Start();
+
+            Thread threadReader = new Thread(new ThreadStart(ReadFib));
+            threadReader.IsBackground = false;
+            threadReader.Start();
+        }
+        private static void ReadFib()
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine("Starting to read from the queue....");
+            
+
+
+            do
+            {
+                var n = numbers.Take();
+                //    if (cq.TryDequeue(out ulong n))
+                //    {
+                        Console.Write("[Fib {0}] ", n);
+                //    }
+                //    else
+                //    {
+                //        //Console.Write(".");
+                //    }
+                //    Thread.Sleep(10);
+
+            } while (true);
+        }
+
+        private static void GenerateFib()
+        {
+            for (ushort ix = 0; ix < 50; ix++)
+            {
+                Thread.Sleep(rnd.Next(1, 500));
+                //ulong fibN = Fibonacci(ix);
+                //cq.Enqueue(fibN);
+                numbers.Add(Fibonacci(ix));
+                Console.WriteLine("Adding next Fib...");
+            }
+        }
+
+        //Fibonacci Sequence
+        private static ulong Fibonacci(ushort n)
+        {
+            return (n == 0) ? 0 : Fib(n);
+        }
+
+        private static ulong Fib(ushort n)
+        {
+            ulong a = 0;
+            ulong b = 1;
+
+            for (uint ix = 0; ix < n - 1; ix++)
+            {
+                ulong next = checked(a + b);
+                a = b;
+                b = next;
+            }
+
+            return b;
+        }
+
+
+    }
+}
